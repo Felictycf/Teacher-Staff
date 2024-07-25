dd494c668616c9123711c127871c8a736f4c8802
Mcsvip <felicitycfgreene@gmail.com>
2024-07-25 12:12:50 +0800

    create about  and contact  compononts

---
+++ client_web/src/components/about.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>About Us</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
