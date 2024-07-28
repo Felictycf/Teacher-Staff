197f21d7a73c4406c181e4eec657c6a94bd225d8
Mcsvip <felicitycfgreene@gmail.com>
2024-07-28 20:03:50 +0800

    create need components , that will use in page

---
+++ client_web/src/components/gallery.js
@@ -0,0 +1,35 @@
+import { Image } from "./image";
+import React from "react";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Teacher photo wall</h2>
+          <p>
+            Class teacher demeanor and campus life and activities
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      smallImage={d.smallImage}
+
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ client_web/src/components/navigation.js
@@ -0,0 +1,67 @@
+import React from "react";
+import logo from '../logo.jpg'
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+
+          <img src={logo} height={70}  alt="Logo" className="logo" />
+          <a className="navbar-brand page-scroll" href="#page-top">
+  </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#features" className="page-scroll">
+                Features
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Teacher photo wall
+              </a>
+            </li>
+
+            <li>
+              <a href="#stafflist" className="page-scroll">
+                StaffList
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a href="#" className="page-scroll">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
